f3bb6b9d0687ff3d60efaabf18e286c8df80ade2
Md Mahathir Ullah Yafi <mahathirullahyafikhan@gmail.com>
2026-05-02 22:28:49 +0600

    Add files via upload

---
+++ Same_Parity_Summands.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+            int n,k;
+            cin>>n>>k;
+            int x=n-(k-1);
+            int y=n-2*(k-1);
+
+            if(x>=1 && x%2==1){
+                cout<<"YES"<<endl;
+                for(int i=1;i<=k-1;i++){
+                    cout<<"1"<<endl;
+                }
+                cout<<x<<endl;
+            }
+            else if(y>=1 && y%2==0){
+                cout<<"YES"<<endl;
+                for(int i=1;i<=k-1;i++){
+                    cout<<"2"<<endl;
+                }
+                cout<<y<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+
+
+
+
+    }
+
+
+}
